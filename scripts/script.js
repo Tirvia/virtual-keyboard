e260b4bee2317df9e947019941da14b57943e3d5
Hanna <fedoska_ann@mail.ru>
2023-05-01 15:31:52 +0300

    feat: add the basis of keyboard

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,53 @@
 document.body.onload = addElement;
   function addElement() {
-    var textArea = document.createElement("textarea");
+    var newDiv = document.createElement("textarea");
     first_div = document.getElementById("none");
-    document.body.insertBefore(textArea, first_div);
+    document.body.insertBefore(newDiv, first_div);
+
+    var keyboardLineFirst = document.createElement("div");
+    keyboardLineFirst.className = "keyboard-line";
+    document.body.append(keyboardLineFirst);  
+    for (let i = 0; i<15; i++){
+        var key = document.createElement("div");
+        key.className = "key";
+        keyboardLineFirst.prepend(key);
+    }     
+
+    var keyboardLineSecond = document.createElement("div");
+    keyboardLineSecond.className = "keyboard-line";
+    document.body.append(keyboardLineSecond);    
+    for (let i = 0; i<16; i++){
+        var key = document.createElement("div");
+        key.className = "key";
+        keyboardLineSecond.prepend(key);
+    }  
+    
+    var keyboardLineThird = document.createElement("div");
+    keyboardLineThird.className = "keyboard-line";
+    document.body.append(keyboardLineThird);    
+    for (let i = 0; i<14; i++){
+        var key = document.createElement("div");
+        key.className = "key";
+        keyboardLineThird.prepend(key);
+    }  
+    
+    var keyboardLineFourth = document.createElement("div");
+    keyboardLineFourth.className = "keyboard-line";
+    document.body.append(keyboardLineFourth);   
+    for (let i = 0; i<14; i++){
+        var key = document.createElement("div");
+        key.className = "key";
+        keyboardLineFourth.prepend(key);
+    }  
+
+    var keyboardLineFifth = document.createElement("div");
+    keyboardLineFifth.className = "keyboard-line";
+    document.body.append(keyboardLineFifth);    
+    for (let i = 0; i<10; i++){
+        var key = document.createElement("div");
+        key.className = "key";
+        keyboardLineFifth.prepend(key);
+    }  
 }
+
+
